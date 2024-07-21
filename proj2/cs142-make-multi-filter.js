5cac20a0392ee2bd37ae17143948f1f94c30dab7
kysshsy <kysshsy@gmail.com>
2024-07-21 16:15:13 +0800

    proj2: pass problem1

---
+++ proj2/cs142-make-multi-filter.js
@@ -0,0 +1,18 @@
+function cs142MakeMultiFilter(originalArray) {
+    let currentArray = originalArray.slice();
+    function arrayFilter(filterCriteria, callback) {
+        if (typeof filterCriteria === 'function') {
+            currentArray = currentArray.filter(item => filterCriteria(item));
+        } else {
+            return currentArray;
+        }
+
+        if (typeof callback === 'function') {
+            callback.call(originalArray, currentArray);
+        }
+
+        return arrayFilter;
+    }
+
+    return arrayFilter;
+}
